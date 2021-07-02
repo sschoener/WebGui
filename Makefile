c3b45778ec3da3a1dc077bc51eac3ee096ce3bc6
Sebastian Schöner <mail@s-schoener.com>
2021-07-02 23:08:13 +0200

    Port to SDL2

--- Makefile
+++ Makefile
@@ -4,12 +4,11 @@ OUTPUT = imgui.js
 IMGUI_DIR:=../imgui
 
 SOURCES = main.cpp
-SOURCES += imgui_impl_glfw.cpp imgui_impl_opengl3.cpp
+SOURCES += imgui_impl_sdl.cpp imgui_impl_opengl3.cpp
 SOURCES += $(IMGUI_DIR)/imgui.cpp $(IMGUI_DIR)/imgui_draw.cpp $(IMGUI_DIR)/imgui_demo.cpp $(IMGUI_DIR)/imgui_widgets.cpp $(IMGUI_DIR)/imgui_tables.cpp
 
 LIBS = -lGL
-WEBGL_VER = -s USE_WEBGL2=1 -s USE_GLFW=3 -s FULL_ES3=1
-#WEBGL_VER = USE_GLFW=2
+WEBGL_VER = -s USE_WEBGL2=1 -s USE_SDL=2 -s FULL_ES3=1
 USE_WASM = -s WASM=1
 
 all: $(SOURCES) $(OUTPUT)
--- main.cpp
+++ main.cpp
@@ -7,169 +7,148 @@
 #define GLFW_INCLUDE_ES3
 #include <GLES3/gl3.h>
 #include <GLFW/glfw3.h>
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
 
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
+#include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl3.h"
 #include <iostream>
 
 
-GLFWwindow* g_window;
+SDL_Window* g_window;
+SDL_GLContext g_gl_context;
 ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 ImGuiContext* imgui = 0;
 bool show_demo_window = true;
 bool show_another_window = false;
 
 EM_JS(int, canvas_get_width, (), {
-  return Module.canvas.width;
+    return Module.canvas.width;
 });
 
 EM_JS(int, canvas_get_height, (), {
-  return Module.canvas.height;
+    return Module.canvas.height;
 });
 
 EM_JS(void, resizeCanvas, (), {
-  js_resizeCanvas();
+    js_resizeCanvas();
 });
 
-void loop()
+void update()
 {
-  int width = canvas_get_width();
-  int height = canvas_get_height();
+    int width = canvas_get_width();
+    int height = canvas_get_height();
+    SDL_SetWindowSize(g_window, width, height);
 
-  glfwSetWindowSize(g_window, width, height);
+    // Poll and handle events (inputs, window resize, etc.)
+    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        ImGui_ImplSDL2_ProcessEvent(&event);
+        //if (event.type == SDL_QUIT)
+        //    done = true;
+        //if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+        //    done = true;
+    }
 
-  ImGui::SetCurrentContext(imgui);
+    static int x = 0;
 
-  ImGui_ImplOpenGL3_NewFrame();
-  ImGui_ImplGlfw_NewFrame();
-  ImGui::NewFrame();
+    x++;
+    if (x > 255)
+        x = 0;
 
-  // 1. Show a simple window.
-  // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
-  {
-      static float f = 0.0f;
-      static int counter = 0;
-      ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+    SDL_GL_MakeCurrent(g_window, g_gl_context);
 
-      ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-      ImGui::Checkbox("Another Window", &show_another_window);
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
 
-      if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-          counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
+    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
 
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-  }
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+    {
+        static float f = 0.0f;
+        static int counter = 0;
 
-  //std::cout << "2nd window" << std::endl;
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
-  // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-  if (show_another_window)
-  {
-      ImGui::Begin("Another Window", &show_another_window);
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me"))
-          show_another_window = false;
-      ImGui::End();
-  }
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+        ImGui::Checkbox("Another Window", &show_another_window);
 
-  // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-  if (show_demo_window)
-  {
-      ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-      ImGui::ShowDemoWindow(&show_demo_window);
-  }
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-  ImGui::Render();
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
 
-  int display_w, display_h;
-  glfwMakeContextCurrent(g_window);
-  glfwGetFramebufferSize(g_window, &display_w, &display_h);
-  glViewport(0, 0, display_w, display_h);
-  glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-  glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
 
-  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-  glfwMakeContextCurrent(g_window);
+    // 3. Show another simple window.
+    if (show_another_window)
+    {
+        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me"))
+            show_another_window = false;
+        ImGui::End();
+    }
+
+    // Rendering
+    ImGui::Render();
+    glViewport(0, 0, width, height);
+    glClearColor(0, x / 255.0 / 2, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    SDL_GL_SwapWindow(g_window);
 }
-
-
-int init()
-{
-  if( !glfwInit() )
-  {
-      fprintf( stderr, "Failed to initialize GLFW\n" );
-      return 1;
-  }
-
-  //glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
-
-  // Open a window and create its OpenGL context
-  int canvasWidth = 800;
-  int canvasHeight = 600;
-  g_window = glfwCreateWindow( canvasWidth, canvasHeight, "WebGui Demo", NULL, NULL);
-  if( g_window == NULL )
-  {
-      fprintf( stderr, "Failed to open GLFW window.\n" );
-      glfwTerminate();
-      return -1;
-  }
-  glfwMakeContextCurrent(g_window); // Initialize GLEW
-
-  // Create game objects
-  // Setup Dear ImGui binding
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO& io = ImGui::GetIO();
-
-  ImGui_ImplGlfw_InitForOpenGL(g_window, false);
-  ImGui_ImplOpenGL3_Init();
-
-  // Setup style
-  ImGui::StyleColorsDark();
-  //ImGui::StyleColorsClassic();
-
-  // Load Fonts
-  io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 23.0f);
-  io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 18.0f);
-  io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 26.0f);
-  io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 32.0f);
-  io.Fonts->AddFontDefault();
-
-  imgui =  ImGui::GetCurrentContext();
-
-  // Cursor callbacks
-  glfwSetMouseButtonCallback(g_window, ImGui_ImplGlfw_MouseButtonCallback);
-  glfwSetScrollCallback(g_window, ImGui_ImplGlfw_ScrollCallback);
-  glfwSetKeyCallback(g_window, ImGui_ImplGlfw_KeyCallback);
-  glfwSetCharCallback(g_window, ImGui_ImplGlfw_CharCallback);
-
-  resizeCanvas();
-
-  return 0;
-}
-
-
-void quit()
-{
-  glfwTerminate();
-}
-
 
 extern "C" int main(int argc, char** argv)
 {
-  if (init() != 0) return 1;
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS) != 0) {
+        printf("Failed to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
 
-  #ifdef __EMSCRIPTEN__
-  emscripten_set_main_loop(loop, 0, 1);
-  #endif
+    g_window = SDL_CreateWindow("Dear ImGui", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 512, 512, SDL_WINDOW_OPENGL);
 
-  quit();
+    g_gl_context = SDL_GL_CreateContext(g_window);
+    
+    {
+        // Setup Dear ImGui binding
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
 
-  return 0;
+        ImGui_ImplSDL2_InitForOpenGL(g_window, g_gl_context);
+        ImGui_ImplOpenGL3_Init("#version 100");
+
+        // Setup style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsClassic();
+
+        // Load Fonts
+        ImGuiIO& io = ImGui::GetIO();
+        io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 23.0f);
+        io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 18.0f);
+        io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 26.0f);
+        io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 32.0f);
+        io.Fonts->AddFontDefault();
+
+        imgui = ImGui::GetCurrentContext();
+    }
+    emscripten_set_main_loop(update, 0, 1);
+    resizeCanvas();
+    return 0;
 }
